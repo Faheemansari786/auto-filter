9984ef6ce26d72ad01cad506c9088300a40dead2
crazy8182 <crazygaming4694@gmail.com>
2026-04-05 11:15:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -83,7 +83,7 @@ def dreamxbotz_plugins_handler(app, plugins_dir: str | Path = "plugins", package
     return loaded_plugins
 
 async def dreamxbotz_start():
-    print('\n\nInitalizing DreamxBotz')
+    print('\n\nInitalizing AKIMAXMOVIESHUB')
     await dreamxbotz.start()
     bot_info = await dreamxbotz.get_me()
     dreamxbotz.username = bot_info.username
